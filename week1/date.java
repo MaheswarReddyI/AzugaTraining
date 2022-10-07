e8ae18edf3904d9447ce5c3ea7dac83fbb123ba0
MaheswarReddyI <112755895+MaheswarReddyI@users.noreply.github.com>
2022-10-07 13:19:22 +0530

    It mimics data command in linux

---
+++ week1/date.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2022.  - All Rights Reserved
+ * Unauthorized copying or redistribution of this file in source and binary forms via any medium
+ * is strictly prohibited-
+ * @Author - indukurimr(Indukuri Maheswar Reddy).
+ */
+
+
+package week1;
+
+/**
+ * This date class mimics the date command in linux
+ */
+
+public class date {
+    public static void main(String[] args) {
+        java.util.Date date = new java.util.Date();
+        System.out.println(date);
+
+    }
+}
