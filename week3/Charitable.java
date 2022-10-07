991d0a104278f1e7460419b7c5a6c9e62a2881ef
MaheswarReddyI <112755895+MaheswarReddyI@users.noreply.github.com>
2022-10-07 13:50:56 +0530

    It Converts csv files to html, xml and pdf

---
+++ week3/Charitable.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022.  - All Rights Reserved
+ * Unauthorized copying or redistribution of this file in source and binary forms via any medium
+ * is strictly prohibited-
+ * @Author - indukurimr(Indukuri Maheswar Reddy).
+ */
+
+package week3;
+
+/**
+ * It creates visual representation of csv Data in the form of charts.
+ */
+
+public interface Charitable {
+    void getBarChart();
+
+    void getLineChart();
+
+    void getPieChart();
+}
